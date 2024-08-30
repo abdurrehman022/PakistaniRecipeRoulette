bc85c0fbecaebcf209539b7272516595e9e09204
Muzzammil Abdur Rehman Akhund <akhundmuzzammil@gmail.com>
2024-08-30 20:53:19 +0500

    Initial backend setup

---
+++ MERN/backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
