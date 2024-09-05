f8790ac2b8bf33807c4ac3a5237b20a58080fa02
Muzzammil Abdur Rehman Akhund <akhundmuzzammil@gmail.com>
2024-09-05 11:16:26 +0500

    Final

--- MERN/backend/app.js
+++ MERN/backend/app.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 
 const app = express();
 
-const cors = require('cors');
 app.use(cors({ origin: 'https://pakistani-recipe-roulette.vercel.app' }));
 
 // Middleware for parsing JSON and URL-encoded data
